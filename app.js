bf03c1cf6d340cce896ea911bb77b5e679e66e9a
Fran Zekan <zekan.fran369@gmail.com>
2021-02-13 22:10:47 +0100

    Setup extension showing data

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const { notify } = require("node-notifier");
 const getBattery = require("./lib/g733");
 
 (async () => {
-  const percentage = await getBattery();
-
-  if (percentage < 90) {
-    notify({
-      title: "Headset battery",
-      message: "G733 battery dropped below 90%",
-    });
+  try {
+    const percentage = await getBattery();
+    console.log(`G733: ${percentage}%`);
+  } catch (err) {
+    console.log(`G733: err`);
   }
+
+  // if (percentage < 90) {
+  //   notify({
+  //     title: "Headset battery",
+  //     message: "G733 battery dropped below 90%",
+  //   });
+  // }
 })();
--- extension/g733@example.com/extension.js
+++ extension/g733@example.com/extension.js
@@ -1,10 +1,41 @@
 // Example #1
+// @ts-nocheck
 
 const { St, Clutter } = imports.gi;
 const Main = imports.ui.main;
-const runShell = imports.misc.util.spawnCommandLine;
+const Mainloop = imports.mainloop;
+const GLib = imports.gi.GLib;
 
 let panelButton;
+let text;
+
+const get = (filename) => {
+  return GLib.spawn_command_line_sync(
+    `cat /home/fran/.cache/${filename}`
+  )[1].toString();
+};
+
+let timeout;
+const func = () => {
+  if (timeout) {
+    Mainloop.source_remove(timeout);
+  }
+
+  let g733 = get("g733");
+  let weather = get("wttrtemp");
+
+  text = new St.Label({ text: `${g733} ${weather}` });
+
+  let monitor = Main.layoutManager.primaryMonitor;
+  text.set_position(
+    monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
+    monitor.y + Math.floor(monitor.height / 2 - text.height / 2)
+  );
+
+  panelButton.set_child(text);
+
+  timeout = Mainloop.timeout_add_seconds(1, func);
+};
 
 function init() {
   // Create a Button with "Hello World" text
@@ -12,14 +43,14 @@ function init() {
     style_class: "panel-button",
   });
 
-  const runShell;
+  func();
 
-  let panelButtonText = new St.Label({
-    text: `G733: 45%`,
+  text = new St.Label({
+    text: `Loading`,
     y_align: Clutter.ActorAlign.CENTER,
   });
 
-  panelButton.set_child(panelButtonText);
+  panelButton.set_child(text);
 }
 
 function enable() {
