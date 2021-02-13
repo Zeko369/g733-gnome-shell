edd14b293977dd49cad234fb5cfffedd24c758bf
Fran Zekan <zekan.fran369@gmail.com>
2021-02-13 21:27:59 +0100

    Play with gnome extensions

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-const { exec } = require("child_process");
 const { notify } = require("node-notifier");
-const { promisify } = require("util");
-
-const execPromise = promisify(exec);
-
-const getBattery = async () => {
-  const res = await execPromise("/usr/local/bin/headsetcontrol -b -c");
-  if (res.stderr) {
-    throw new Error("Error reading data");
-  }
-
-  const val = parseInt(res.stdout);
-
-  if (isNaN(val)) {
-    throw new Error(res.stdout);
-  }
-
-  return val;
-};
+const getBattery = require("./lib/g733");
 
 (async () => {
   const percentage = await getBattery();
---
+++ extension/g733@example.com/extension.js
@@ -0,0 +1,33 @@
+// Example #1
+
+const { St, Clutter } = imports.gi;
+const Main = imports.ui.main;
+const runShell = imports.misc.util.spawnCommandLine;
+
+let panelButton;
+
+function init() {
+  // Create a Button with "Hello World" text
+  panelButton = new St.Bin({
+    style_class: "panel-button",
+  });
+
+  const runShell;
+
+  let panelButtonText = new St.Label({
+    text: `G733: 45%`,
+    y_align: Clutter.ActorAlign.CENTER,
+  });
+
+  panelButton.set_child(panelButtonText);
+}
+
+function enable() {
+  // Add the button to the panel
+  Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+}
+
+function disable() {
+  // Remove the added button from panel
+  Main.panel._rightBox.remove_child(panelButton);
+}
